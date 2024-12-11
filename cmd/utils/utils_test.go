0a848357f8702bc9743a799f66272b7a1dc4c679
Filippo Trotter <filippotrotter@MBPdiFilippo2.fritz.box>
2024-12-11 12:12:30 +0100

    fix: utils test

--- cmd/utils/utils_test.go
+++ cmd/utils/utils_test.go
@@ -155,23 +155,25 @@ func TestLoadMetadata(t *testing.T) {
 	sampleMetadata := CommandMetadata{
 		Description: "Test description",
 		Arguments: []struct {
-			Name        string `json:"name"`
-			Description string `json:"description,omitempty"`
-			Type        string `json:"type,omitempty"`
+			Name        string                 `json:"name"`
+			Description string                 `json:"description,omitempty"`
+			Type        string                 `json:"type,omitempty"`
+			Properties  map[string]interface{} `json:"properties,omitempty"`
 		}{
 			{Name: "arg1", Description: "Argument 1 description"},
 			{Name: "arg2", Description: "Argument 2 description", Type: "integer"},
 		},
 		Options: []struct {
-			Name        string   `json:"name"`
-			Description string   `json:"description,omitempty"`
-			Default     string   `json:"default,omitempty"`
-			Choices     []string `json:"choices,omitempty"`
-			Env         []string `json:"env,omitempty"`
-			Hidden      bool     `json:"hidden,omitempty"`
-			File        bool     `json:"file,omitempty"`
-			RawData     bool     `json:"rawdata,omitempty"`
-			Type        string   `json:"type,omitempty"`
+			Name        string                 `json:"name"`
+			Description string                 `json:"description,omitempty"`
+			Default     string                 `json:"default,omitempty"`
+			Choices     []string               `json:"choices,omitempty"`
+			Env         []string               `json:"env,omitempty"`
+			Hidden      bool                   `json:"hidden,omitempty"`
+			File        bool                   `json:"file,omitempty"`
+			RawData     bool                   `json:"rawdata,omitempty"`
+			Type        string                 `json:"type,omitempty"`
+			Properties  map[string]interface{} `json:"properties,omitempty"`
 		}{
 			{Name: "--option1, -o", Description: "Option 1 description", Default: "default1", Choices: []string{"choice1", "choice2"}, Type: "string"},
 		},
@@ -262,23 +264,25 @@ func TestConfigureArgumentsAndFlags(t *testing.T) {
 	metadata := &CommandMetadata{
 		Description: "Test command",
 		Arguments: []struct {
-			Name        string `json:"name"`
-			Description string `json:"description,omitempty"`
-			Type        string `json:"type,omitempty"`
+			Name        string                 `json:"name"`
+			Description string                 `json:"description,omitempty"`
+			Type        string                 `json:"type,omitempty"`
+			Properties  map[string]interface{} `json:"properties,omitempty"`
 		}{
 			{Name: "<arg1>", Description: "Required argument"},
 			{Name: "[arg2]", Description: "Optional argument"},
 		},
 		Options: []struct {
-			Name        string   `json:"name"`
-			Description string   `json:"description,omitempty"`
-			Default     string   `json:"default,omitempty"`
-			Choices     []string `json:"choices,omitempty"`
-			Env         []string `json:"env,omitempty"`
-			Hidden      bool     `json:"hidden,omitempty"`
-			File        bool     `json:"file,omitempty"`
-			RawData     bool     `json:"rawdata,omitempty"`
-			Type        string   `json:"type,omitempty"`
+			Name        string                 `json:"name"`
+			Description string                 `json:"description,omitempty"`
+			Default     string                 `json:"default,omitempty"`
+			Choices     []string               `json:"choices,omitempty"`
+			Env         []string               `json:"env,omitempty"`
+			Hidden      bool                   `json:"hidden,omitempty"`
+			File        bool                   `json:"file,omitempty"`
+			RawData     bool                   `json:"rawdata,omitempty"`
+			Type        string                 `json:"type,omitempty"`
+			Properties  map[string]interface{} `json:"properties,omitempty"`
 		}{
 			{Name: "--flag1", Description: "Test flag", Default: "default_value"},
 		},
